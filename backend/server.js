f80a1b78ccb7a0fa40c1c431c343a6ce558a4b51
Varvara Moiseenko <moiseencoo@gmail.com>
2025-08-04 15:54:36 +0500

    vercel config

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 // const jsonServer = require('json-server')
-// const cors = require('cors')
 
 // const server = jsonServer.create()
 // const router = jsonServer.router('db.json')
 // const middlewares = jsonServer.defaults()
 
-// // Enable CORS for all routes
-// server.use(cors())
+
 
 // // Use default middlewares (logger, static, cors and no-cache)
 // server.use(middlewares)
@@ -19,19 +17,23 @@
 
 // // Start server (only if not in Vercel)
 // if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
-//   const port = process.env.PORT || 2311
-//   server.listen(port, () => {
-//     console.log(`JSON Server is running on port ${port}`)
-//   })
-// } 
-
-// JSON Server module
+    //   const port = process.env.PORT || 2311
+    //   server.listen(port, () => {
+        //     console.log(`JSON Server is running on port ${port}`)
+        //   })
+        // } 
+        
+        // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require('cors')
 
 server.use(middlewares);
+
+// Enable CORS for all routes
+server.use(cors())
 server.use(router);
 server.listen(2311, () => {
 	console.log("JSON Server is running");
